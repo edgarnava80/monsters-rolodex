086cfdba16b75e6d341928215174a7f1dba4f38d
edgarnava80 <edgarnr@hotmail.com>
2020-07-16 10:12:42 -0500

    Updates

---
+++ precache-manifest.10e0f711f37fda039b26358dc2fa2888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70f7e54022f2359827b29c69c754aee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a9168ba585bc822f9959",
+    "url": "/monsters-rolodex/static/css/main.fb0de570.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9168ba585bc822f9959",
+    "url": "/monsters-rolodex/static/js/main.57ea14b4.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
